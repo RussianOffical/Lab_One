61ce1355a1a695f3684fab380ac5c96199745eea
Student <student@ECCO-COW-D04-1297899.local>
2017-01-19 14:01:41 -0800

    init commit

--- p.py
+++ p.py
@@ -1,5 +1,23 @@
-x = input("Input1")
-y = input("Input2")
-if type(x) == type(y):
-    print type(x)
-    print x + y
+while True:
+    a = input("ADDITION = 1 or SUBTRACTION = 2")
+    if a == 1:
+        print "ADDITION"
+        x = input("First value")
+        y = input("Second value")
+        if type(x) == type(y):
+            print type(x)
+            print x + y
+        else:
+            print "Addition is unspecified for the input types given"
+            print "*HINT* Try using the same type"
+    if a == 2:
+        print "SUBTRACTION"
+        x = input("First value")
+        y = input("Second value")
+        if type(x) == type(y):
+            print type(x)
+            print x - y
+        else:
+            print "Addition is unspecified for the input types given"
+            print "*HINT* Try using the same type"
+
